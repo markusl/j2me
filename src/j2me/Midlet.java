4811f519c49e94a69d4198731019a06f77493cbe
Markus Lindqvist <markus.lindqvist@iki.fi>
2013-04-17 16:30:19 +0300

    Project layout

---
+++ src/j2me/Midlet.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j2me;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author markus
+ */
+public class Midlet extends MIDlet {
+
+    public void startApp() {
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
